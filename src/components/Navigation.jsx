55441a3354225932448b753bcbc5739daee294d9
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-05-12 21:47:13 +0900

    navigate design

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,16 @@ import { NavLink } from "react-router-dom";
 export default function Navigation() {
   return (
     <>
+     <div className={"w-[1434px] h-[80px] bg-white bg-opacity-90 top-0 fixed"}>
       <div className={"flex gap-5"}>
-        <NavLink to={"/"}>홈</NavLink>
+      <NavLink to={"/"} className={"ml-[72px] mt-[25.5px] text-[24px] text-main font-bold"}>동아리 로고</NavLink>
+      <div className ={"space-x-[80px] text-sm ml-[48px] mt-[31px]"}>
         <NavLink to={"/club"}>동아리</NavLink>
+        <NavLink to={"/promotion"}>홍보게시판</NavLink>
         <NavLink to={"/notice"}>공지사항</NavLink>
-        <NavLink to={"/promotion"}>홍보</NavLink>
+        
+        </div>
+      </div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+      extend:{
+        colors: {
+          'main' : '#4200FF',
+          'textColor' : '#4F4F4F',
+          'buttonColorM': '#6360FF',
+          'buttonColorS' : '#ECEDFB',
+          'tegColor' : '#F4F4F4',
+          'noticeColor' : '#F1F2FC',
+          'opacity': 'bg-opacity-100'
+        },
+        
+      }
+      
   },
   plugins: [],
-};
+}
