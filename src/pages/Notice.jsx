35656168355c70cb6c0dfb3690aba16f84b52544
Choguesung <tkdgh1135@naver.com>
2023-07-05 13:53:34 +0900

    hello

--- src/pages/Notice.jsx
+++ src/pages/Notice.jsx
@@ -16,7 +16,7 @@ const Notice = () => {
     setIsCategoryOpen(false);
   };
 
-  // 게시글 목록 데이터 여기서 주석을 수정하면 어떤일이 일어날까요?
+  // 게시글 목록 데이터 여기서 주석을 수정하면 어떤일이 일어날까요?용용
   const posts = [
     { id: 1, title: '게시글 1', author: '작성자 1', date: '2023-06-30' },
     { id: 2, title: '게시글 2', author: '작성자 2', date: '2023-07-01' },
