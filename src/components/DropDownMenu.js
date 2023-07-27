4266f73a000c15086af8a4f9c41d791eceb8c6c3
rusia9217 <110519493+unnimm@users.noreply.github.com>
2023-07-27 14:18:51 +0900

    feat: 동아리 가입 신청 양식 설정
    동아리 가입 신청 양식 설정

--- src/components/DropDownMenu.js
+++ src/components/DropDownMenu.js
@@ -1,10 +1,10 @@
 // DropdownMenu.js
 import React, { useState } from 'react';
 
-const DropdownMenu = () => {
+const DropdownMenu = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleMenuClick = () => {
+  const handleMenuClick = (props) => {
     setIsOpen(!isOpen);
   };
 
@@ -18,9 +18,20 @@ const DropdownMenu = () => {
       </button>
       {isOpen && (
         <div className="absolute mt-2 bg-gray3 rounded-lg shadow-lg">
-          <a href="#" className="block rounded-lg  py-2 px-4 text-h5 hover:bg-gray">메뉴 항목 1</a>
-          <a href="#" className="block rounded-lg  py-2 px-4 text-h5 hover:bg-gray">메뉴 항목 2</a>
-          <a href="#" className="block rounded-lg  py-2 px-4 text-h5 hover:bg-gray">메뉴 항목 3</a>
+          <a href="#" className="block rounded-lg  py-2 px-4 text-h5 hover:bg-gray"
+          onClick={()=>{
+            let copy = props.type
+            copy = 0
+            props.setType(copy)
+            console.log(props.type)
+          }}>주관식</a>
+          <a href="#" className="block rounded-lg  py-2 px-4 text-h5 hover:bg-gray"
+          onClick={()=>{
+            let copy = props.type
+            copy = 1
+            props.setType(copy)
+            console.log(props.type)
+          }}>첨부파일</a>
         </div>
       )}
     </div>
--- src/pages/clubdetail/ClubSignUp.jsx
+++ src/pages/clubdetail/ClubSignUp.jsx
@@ -47,9 +47,9 @@ export default function ClubSignUp() {
   }, []);
   return (
     <>
-      {/* <UserSignUp formData={formData} id={id} listData={listData} /> */}
+      <UserSignUp formData={formData} id={id} listData={listData} />
       <UserApplicationForm formData={formData} listData={listData} id={id} objid={objid}/>
-      {/* <ManagerSignUp /> */}
+      <ManagerSignUp />
     </>
   );
 }
